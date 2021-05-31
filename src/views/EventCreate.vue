480ff0ec674cd588006623fdbfadac29b3d728b4
yxj0312 <yanxiajun@hotmail.com>
2021-05-31 22:58:07 +0200

    create uuid

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -44,6 +44,8 @@
 </template>
 
 <script>
+import { v4 as uuidv4 } from 'uuid'
+
 export default {
   data() {
     return {
@@ -70,6 +72,7 @@ export default {
   },
   methods: {
     onSubmit() {
+      this.event.id = uuidv4()
       this.event.organizer = this.$store.state.user
       console.log('Event:', this.event)
     }
