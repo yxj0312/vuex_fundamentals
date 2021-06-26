cfdaf498526f1d995aace7f634185e1ed00d555c
yxj0312 <yanxiajun@hotmail.com>
2021-06-26 21:41:52 +0200

    reafactor EventList and EventCard so that the test passes

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,13 +1,13 @@
 <template>
-  <router-link
+  <!-- <router-link
     class="event-link"
     :to="{ name: 'EventDetails', params: { id: event.id } }"
-  >
-    <div class="event-card">
-      <span>@{{ event.time }} on {{ event.date }}</span>
-      <h4>{{ event.title }}</h4>
-    </div>
-  </router-link>
+  > -->
+  <div class="event-card">
+    <span>@{{ event.time }} on {{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
+  </div>
+  <!-- </router-link> -->
 </template>
 
 <script>
